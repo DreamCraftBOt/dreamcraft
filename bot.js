e983c4666e611321e1fc150f0663315f056f0d56
DreamCraftBOt <46896260+DreamCraftBOt@users.noreply.github.com>
2019-01-21 18:09:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,29 @@ client.user.setGame(``,"http://twitch.tv/S-F")
 
 
 
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('&bc')) {
+          if (!args[0]) {
+message.channel.send("**+bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
+
 
 
 client.on('message', message => {
